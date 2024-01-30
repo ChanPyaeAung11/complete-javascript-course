468a39e73a0a78661bcd0d5787e3cc33853c6e10
Chan <chanpyaeag11@gmail.com>
2024-01-30 17:30:56 +0800

    DOM manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+// secret number
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+// DOM is not or part of JS
+// DOM is created by browsers and have methods of DOM being accessed by web API
+
+// accessing web api and DOM.
+console.log(document.querySelector('.message').textContent);
+// manipulate DOM
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.score').textContent = 10;
+
+// JS input this value into element with this class
+document.querySelector('.guess').value = 23;
+
+function printValue() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🟥 No Number!';
+  } else if (guess === number) {
+  }
+}
+document.querySelector('.check').addEventListener('click', printValue);
