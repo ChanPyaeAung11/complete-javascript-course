9631805882fd527258ce9290888b7a1f5c70918b
Chan <chanpyaeag11@gmail.com>
2024-02-19 13:23:53 +0800

    hoist and TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -5,9 +5,18 @@ function calcAge(birthYear) {
   // birthYear is only in current scope and accessible within this scop
   const age = 2037 - birthYear;
 
-  console.log(firstName);
+  // cuz of var declaration, will be able to access and assign values b4 being declared
+  var3 = 'hello';
+  console.log(var3);
+  var var3;
+
+  let var2;
+  console.log(var2, var3); //var3 will retain the value
+  console.log(undeclaredVar); //will return RefError not defined
+
+  // console.log(firstName); will find firstName from current scope. if var, undefined. if others, RefError
   // variables with same names can be redeclared in any scope.
-  const firstName = 'Sue';
+  var firstName = 'Sue';
 
   function printAge() {
     // variables with same names can be redeclared in any scope.
@@ -29,7 +38,6 @@ function calcAge(birthYear) {
       function add(a, b) {
         // variables with same names can be redeclared in any scope.
         const firstName = 'Sue';
-
         return a + b;
       }
       console.log(add(2, 3));
@@ -46,4 +54,46 @@ function calcAge(birthYear) {
 }
 
 const firstName = 'Jonas';
-calcAge(1991);
+//calcAge(1991);
+
+// hoisting exploration
+console.log(x);
+// console.log(y);
+// console.log(z);
+var x;
+let y;
+const z = 10;
+
+console.log(y);
+console.log(z);
+
+doNth();
+
+console.log(doNth1);
+//doNth1(); // TypeError: Not a function || cause: var is still undefined
+// console.log(doNth2()); // RefError: Cannot use before initlization
+function doNth() {
+  console.log('DO NTH FUNCTION');
+}
+
+var doNth1 = function (a, b) {
+  console.log('do nth 1');
+};
+
+const doNth2 = () => {
+  console.log('do nth 2');
+};
+
+// hoisting's downfall
+
+// value is undefined which is falsy value
+if (numOfItems) deleteShoppingCart();
+var numOfItems = 10;
+
+function deleteShoppingCart() {
+  console.log('all items deleted');
+}
+
+// vars declared using var are all in window - global object in js
+console.log(x === window.x);
+console.log(numOfItems === window.numOfItems);
