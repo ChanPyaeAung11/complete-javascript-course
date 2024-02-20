c3739ced55f89446a01b5a69d1540cb8f8a9ee88
Chan <chanpyaeag11@gmail.com>
2024-02-20 16:28:26 +0800

    destructuring arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,44 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (index1, index2) {
+    return [this.starterMenu[index1], this.starterMenu[index2]];
+  },
 };
+
+// normal way of getting elements from array or object
+const menu1 = restaurant.mainMenu[0];
+const menu2 = restaurant.mainMenu[1];
+const menu3 = restaurant.mainMenu[2];
+console.log(menu1, menu2, menu3);
+
+// destructuring arrays from ES6
+// this is not an array. if js engine sees [] on left side, it knows there will be destructuring
+const [x, y, z] = restaurant.mainMenu;
+console.log(x, y, z);
+
+// can destructure partial
+let [main, , secondary] = restaurant.starterMenu;
+console.log(main, secondary);
+
+// normally to switch values between 2 vars require temp var
+const temp = main;
+main = secondary;
+secondary = temp;
+
+// with destructruing to switch values
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// returned array can be divided into diff variables w/o using array's indices
+console.log(restaurant.order(0, 2));
+const [item1, item2] = restaurant.order(0, 2);
+console.log(item1, item2);
+
+// destructuring nested arrays
+const [j, , [f, k]] = [5, 4, [6, 9]];
+console.log(j, f, k);
+
+// default values assigned. in case not know the array
+const [m = null, u = null, c = null] = [5, 6];
+console.log(m, u, c);
