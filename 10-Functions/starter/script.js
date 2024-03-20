4bc874e1a80ac9dbb8eca56973363021bc2d9355
Chan <chanpyaeag11@gmail.com>
2024-03-20 16:23:25 +0800

    pass by value

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -21,3 +21,28 @@ createBooking(`LH123`, 10, 100);
 createBooking(`LH123`, 10);
 createBooking(`LH123`, 5);
 createBooking(`LH123`, undefined, 5); // undefined === not setting the value
+
+const flight = `LH234`;
+const jonas = {
+  name: `Jonas Shit`,
+  passport: 234323,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = `LH999`;
+  passenger.name = `Mr. ` + passenger.name;
+
+  if (passenger.passport === 234323) {
+    alert(`Checked in`);
+  } else {
+    alert(`Wrong passport`);
+  }
+};
+checkIn(flight, jonas);
+console.log(flight, jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
