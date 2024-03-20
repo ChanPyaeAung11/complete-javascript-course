51cc0a7ea4823d496f209a0806e69934d8c127b6
Chan <chanpyaeag11@gmail.com>
2024-03-20 14:37:41 +0800

    default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum = 123,
+  numPassengers = 1,
+  price = 199 * numPassengers // ONLY worked on paramters defined b4
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking(`LH123`);
+createBooking(`LH123`, 10, 100);
+createBooking(`LH123`, 10);
+createBooking(`LH123`, 5);
+createBooking(`LH123`, undefined, 5); // undefined === not setting the value
